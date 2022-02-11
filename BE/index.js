65703ef4858fd30ec6597a6a3e9f5483a9403b66
Ali Nabilah Ramadhan <alinabilahramadhan@gmail.com>
2022-02-11 11:06:52 +0700

    first app with express js

---
+++ BE/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import db from "./config/database.js";
+import productRoutes from "./routes/index.js";
+
+const app = express();
+
+try {
+    await db.authenticate();
+    console.log('Database Tersambung');
+} catch (error) {
+    console.error('Gagal Tersambung Karena: ', error);
+}
+
+app.use('/product', productRoutes);
+
+app.listen(5000, () => console.log('server running at port 5000'));
